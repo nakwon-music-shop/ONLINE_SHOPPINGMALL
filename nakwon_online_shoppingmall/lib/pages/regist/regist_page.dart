4ad9cd323b77b55e83e1e9b735dac2f586f7e6be
marion419 <daynvi@gmail.com>
2024-11-21 15:15:38 +0900

    [add]등록 페이지 appBar 추가

--- nakwon_online_shoppingmall/lib/pages/regist/regist_page.dart
+++ nakwon_online_shoppingmall/lib/pages/regist/regist_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 class RegistPage extends StatelessWidget {
+  RegistPage({super.key});
+
   @override
   Widget build(Object context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('Nakwon'),
+        centerTitle: true,
+      ),
+    );
   }
 }
