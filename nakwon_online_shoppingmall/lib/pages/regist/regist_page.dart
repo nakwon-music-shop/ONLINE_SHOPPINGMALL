3cf6144204d24285e908f8ef589f27e4dc4acf37
marion419 <daynvi@gmail.com>
2024-11-22 19:47:44 +0900

    [add]TextField에서 값 받아오는 기능 추가

--- nakwon_online_shoppingmall/lib/pages/regist/regist_page.dart
+++ nakwon_online_shoppingmall/lib/pages/regist/regist_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:nakwon_online_shoppingmall/album.dart';
 import 'dart:io';
 import 'package:nakwon_online_shoppingmall/pages/regist/widgets/background_photo.dart';
 import 'package:nakwon_online_shoppingmall/pages/regist/widgets/info_textField.dart';
@@ -7,13 +8,17 @@ import 'package:nakwon_online_shoppingmall/pages/regist/widgets/jaket_photo_butt
 
 class RegistPage extends StatefulWidget {
   RegistPage({super.key});
+  String? song;
+  String? artist;
+  int? price;
+  String? description;
 
   @override
   State<RegistPage> createState() => _RegistPageState();
 }
 
 class _RegistPageState extends State<RegistPage> {
-  XFile? image;
+  XFile? image; // 업로드하는 이미지를 저장하는 변수
 
   @override
   Widget build(Object context) {
@@ -24,6 +29,7 @@ class _RegistPageState extends State<RegistPage> {
       ),
       body: Column(
         children: [
+          // 이미지 업로드 영역
           Stack(
             alignment: Alignment.center,
             children: [
@@ -39,15 +45,32 @@ class _RegistPageState extends State<RegistPage> {
           const SizedBox(
             height: 20,
           ),
-          InfoTextfield(data: '가수'),
-          InfoTextfield(data: '상품명'),
-          InfoTextfield(data: '상품 가격'),
-          InfoTextfield(data: '상품 설명'),
+          InfoTextfield(
+            data: '가수',
+            textFieldResult: widget.artist,
+            onTextChanged: onArtistChanged,
+          ),
+          InfoTextfield(
+            data: '상품명',
+            textFieldResult: widget.song,
+            onTextChanged: onSongChanged,
+          ),
+          InfoTextfield(
+            data: '상품 가격',
+            textFieldResult: widget.price,
+            onTextChanged: onPriceChanged,
+          ),
+          InfoTextfield(
+            data: '상품 설명',
+            textFieldResult: widget.description,
+            onTextChanged: onDescriptionChanged,
+          ),
         ],
       ),
     );
   }
 
+  // ImagePicker
   Future<void> getImagePickerData() async {
     ImagePicker picker = ImagePicker();
     XFile? pickedImage = await picker.pickImage(source: ImageSource.gallery);
@@ -56,5 +79,33 @@ class _RegistPageState extends State<RegistPage> {
         image = pickedImage;
       });
     }
+  }
+
+  // 상품명 setState
+  void onSongChanged(TextEditingController tec) {
+    setState(() {
+      widget.song = tec.text;
+    });
+  }
+
+  // 가수 setState
+  void onArtistChanged(TextEditingController tec) {
+    setState(() {
+      widget.artist = tec.text;
+    });
+  }
+
+  // 가격 setState
+  void onPriceChanged(TextEditingController tec) {
+    setState(() {
+      widget.price = int.parse(tec.text);
+    });
+  }
+
+  // 상품 설명 setState
+  void onDescriptionChanged(TextEditingController tec) {
+    setState(() {
+      widget.description = tec.text;
+    });
   }
 }
