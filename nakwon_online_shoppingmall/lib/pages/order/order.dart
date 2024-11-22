496f59dbd4b593a3db71d542bc7a26e5a785d55e
suhyunChoo <s_where@naver.com>
2024-11-22 15:44:56 +0900

    [add] StatfulWidget으로 변경

---
+++ nakwon_online_shoppingmall/lib/pages/order/order.dart
@@ -0,0 +1,13 @@
+class Order {
+  final String imagePath;
+  final String song;
+  final String singer;
+  final int price;
+
+  Order({
+    required this.imagePath,
+    required this.song,
+    required this.singer,
+    required this.price,
+  });
+}
