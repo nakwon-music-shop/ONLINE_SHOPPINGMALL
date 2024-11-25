f04c1d370112da51d5b20e8467ccb57cec290312
marion419 <daynvi@gmail.com>
2024-11-25 20:38:18 +0900

    [modify]버튼 파일 분리

--- nakwon_online_shoppingmall/lib/pages/regist/regist_page.dart
+++ nakwon_online_shoppingmall/lib/pages/regist/regist_page.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:nakwon_online_shoppingmall/album.dart';
-import 'dart:io';
 import 'package:nakwon_online_shoppingmall/pages/regist/widgets/background_photo.dart';
 import 'package:nakwon_online_shoppingmall/pages/regist/widgets/info_textField.dart';
 import 'package:nakwon_online_shoppingmall/pages/regist/widgets/jaket_photo_button.dart';
+import 'package:nakwon_online_shoppingmall/pages/regist/widgets/regist_button.dart';
 
 class RegistPage extends StatefulWidget {
   RegistPage({super.key});
@@ -36,100 +34,59 @@ class _RegistPageState extends State<RegistPage> {
         ),
       ),
       // 상품 이미지가 들어가는 부분
-      body: Padding(
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          children: [
-            // 이미지 업로드 영역
-            Stack(
-              alignment: Alignment.center,
+      body: Column(
+        children: [
+          // 이미지 업로드 영역
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              BackgroundPhoto(
+                image: image,
+              ),
+              JaketPhotoButton(
+                image: image,
+                getImagePickerData: getImagePickerData,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // 상품 정보를 입력하는 scrollView
+          Expanded(
+              child: SingleChildScrollView(
+            child: Column(
               children: [
-                BackgroundPhoto(
-                  image: image,
+                // 가수 입력 영역
+                InfoTextfield(
+                  data: '가수',
+                  textFieldResult: widget.artist,
+                  onTextChanged: onArtistChanged,
                 ),
-                JaketPhotoButton(
-                  image: image,
-                  getImagePickerData: getImagePickerData,
+                // 상품명 입력 영역
+                InfoTextfield(
+                  data: '상품명',
+                  textFieldResult: widget.song,
+                  onTextChanged: onSongChanged,
+                ),
+                // 상품 가격 입력 영역
+                InfoTextfield(
+                  data: '상품 가격',
+                  textFieldResult: widget.price,
+                  onTextChanged: onPriceChanged,
+                ),
+                // 상품 설명 입력 영역
+                InfoTextfield(
+                  data: '상품 설명',
+                  textFieldResult: widget.description,
+                  onTextChanged: onDescriptionChanged,
                 ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            // 상품 정보를 입력하는 scrollView
-            Expanded(
-                child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  // 가수 입력 영역
-                  InfoTextfield(
-                    data: '가수',
-                    textFieldResult: widget.artist,
-                    onTextChanged: onArtistChanged,
-                  ),
-                  // 상품명 입력 영역
-                  InfoTextfield(
-                    data: '상품명',
-                    textFieldResult: widget.song,
-                    onTextChanged: onSongChanged,
-                  ),
-                  // 상품 가격 입력 영역
-                  InfoTextfield(
-                    data: '상품 가격',
-                    textFieldResult: widget.price,
-                    onTextChanged: onPriceChanged,
-                  ),
-                  // 상품 설명 입력 영역
-                  InfoTextfield(
-                    data: '상품 설명',
-                    textFieldResult: widget.description,
-                    onTextChanged: onDescriptionChanged,
-                  ),
-                ],
-              ),
-            )),
-            // 상품 등록하기 버튼
-            Container(
-              padding: EdgeInsets.all(20),
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () {
-                    if (widget.song != null &&
-                        widget.artist != null &&
-                        widget.price != null &&
-                        widget.description != null) {
-                      Navigator.pop(
-                          context,
-                          Album(
-                              imagePath: image!.path,
-                              song: widget.song!,
-                              artist: widget.artist!,
-                              price: widget.price!,
-                              description: widget.description!));
-                    } else {
-                      showCupertinoDialog(
-                          context: context,
-                          builder: (context) => CupertinoAlertDialog(
-                                title: Text('정보를 빠짐없이 입력해주세요.'),
-                                actions: <CupertinoDialogAction>[
-                                  CupertinoDialogAction(
-                                    isDefaultAction: true,
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text(
-                                      '확인',
-                                      style: TextStyle(color: Colors.blue),
-                                    ),
-                                  )
-                                ],
-                              ));
-                    }
-                  },
-                  child: Text('상품 등록하기')),
-            )
-          ],
-        ),
+          )),
+          // 상품 등록하기 버튼
+          RegistButton(image: image, widget: widget)
+        ],
       ),
     );
   }
---
+++ nakwon_online_shoppingmall/lib/pages/regist/widgets/regist_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:nakwon_online_shoppingmall/album.dart';
+import 'package:nakwon_online_shoppingmall/pages/regist/regist_page.dart';
+
+class RegistButton extends StatelessWidget {
+  const RegistButton({
+    super.key,
+    required this.image,
+    required this.widget,
+  });
+
+  final XFile? image;
+  final RegistPage widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      child: ElevatedButton(
+          onPressed: () {
+            // 데이터를 모두 입력한 경우
+            if (image != null &&
+                widget.song != null &&
+                widget.artist != null &&
+                widget.price != null &&
+                widget.description != null) {
+              // Album 객체 생성해서 pop
+              Navigator.pop(
+                  context,
+                  Album(
+                      imagePath: image!.path,
+                      song: widget.song!,
+                      artist: widget.artist!,
+                      price: widget.price!,
+                      description: widget.description!));
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('상품을 등록하였습니다'),
+                ),
+              );
+              // 데이터를 모두 입력하지 않은 경우 경고문 출력
+            } else {
+              showCupertinoDialog(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                        title: Text('정보를 빠짐없이 입력해주세요.'),
+                        actions: <CupertinoDialogAction>[
+                          CupertinoDialogAction(
+                            isDefaultAction: true,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              '확인',
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          )
+                        ],
+                      ));
+            }
+          },
+          child: Text('상품 등록하기')),
+    );
+  }
+}
