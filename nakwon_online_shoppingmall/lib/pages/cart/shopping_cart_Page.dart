9f4e0bd56a638393b9aa85c5d30eafb00afd7644
0Ca-t0 <rlfls5975@gmail.com>
2024-11-25 15:26:18 +0900

    [ADD]cartFeature5

--- nakwon_online_shoppingmall/lib/pages/cart/shopping_cart_Page.dart
+++ nakwon_online_shoppingmall/lib/pages/cart/shopping_cart_Page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //AppBar, Column(CartList), ElevatedButton으로 구성
 //기본 폰트 : 16 + grey[700], padding 좌우로 20
@@ -5,10 +6,6 @@ import 'package:flutter/material.dart';
 //TODO 담은 상품 사진 가져오기
 //TODO 담은 상품 제품명 가져오기
 //TODO 담은 상품 수량 가져오기
-//TODO 담은 상품 X버튼을 눌러 카트에서 빼기
-//TODO 상품 추가하고 빼기 버튼 구현
-//TODO 구매하기 버튼 구현
-//TODO 아무것도 담지 않았을 때 '장바구니가 비었습니다.' 나오게 하기
 
 class ShoppingCartPage extends StatefulWidget {
   const ShoppingCartPage({super.key});
@@ -36,17 +33,60 @@ class ShoppingCartState extends State<ShoppingCartPage> {
   }
 
   void decreaseQuantity(int index) {
-    //수량 감소
     setState(() {
-      cartItems[index]['quantity']--;
+      if (cartItems[index]['quantity'] > 1) {
+        cartItems[index]['quantity']--;
+      } else {
+        //수량이 1에서 0이 될 때 상품 제거
+        cartItems.removeAt(index);
+      }
     });
   }
 
   void removeCartList(int index) {
-    //장바군 제거
+    //장바구니 제거
     setState(() {
       cartItems.removeAt(index);
     });
+  }
+
+  void showDialog() {
+    int total = 0;
+    for (var item in cartItems) {
+      total += (item['price'] as int) * (item['quantity'] as int);
+    } //총 금액 계산
+
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          content: Text('총 ${total}원을 결제하시겠습니까?'),
+          actions: [
+            CupertinoDialogAction(
+              onPressed: () {
+                Navigator.pop(context); // 다이얼로그 닫기
+              },
+              child: const Text(
+                '취소',
+                style: TextStyle(color: Colors.redAccent),
+              ),
+            ),
+            CupertinoDialogAction(
+              onPressed: () {
+                Navigator.pop(context); // 다이얼로그 닫기
+                Navigator.pop(context);
+              },
+              child: const Text(
+                '구매하기',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -54,106 +94,120 @@ class ShoppingCartState extends State<ShoppingCartPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Cart'),
-        bottom: PreferredSize(
-            //선 그어주기
-            preferredSize: const Size.fromHeight(1),
-            child: Container(
-              color: Colors.black,
-              height: 1,
-            )),
+        //앱바
+        title: Text(
+          'Cart',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.black,
       ),
       body: Padding(
         padding: EdgeInsets.all(5),
         child: Column(
           children: [
             Expanded(
-              child: ListView.builder(
-                itemCount: cartItems.length, //리스트에 담겨 있는 아이템 갯수만큼 생성해주기
-                itemBuilder: (context, index) {
-                  final item = cartItems[index];
-                  return Card(
-                    //테두리를 사용하고 기본 레이아웃을 사용해 UI 구성
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        side: const BorderSide(color: Colors.black38, width: 1),
-                        borderRadius: BorderRadius.circular(10)),
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 20), //카드 여백
-                    child: ListTile(
-                      leading: item['image'] != null //왼쪽 이미지
-                          ? Image.file(
-                              item['image']!, //링크를 사용 할 경우 network로 바꿔서 사용
-                              width: 50,
-                              height: 50,
-                              fit: BoxFit.cover)
-                          : const Icon(
-                              //이미지 파일이 없을경우
-                              Icons.photo,
-                              size: 50,
-                              color: Colors.grey,
-                            ),
-                      title: Text(item['title']),
-                      subtitle: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            onPressed: () => decreaseQuantity(index),
-                            icon: const Icon(Icons.remove),
-                          ),
-                          //감소 버튼
-                          Text('${item['quantity']}'), //수량
-                          IconButton(
-                            onPressed: () => increaseQuantity(index),
-                            icon: const Icon(Icons.add),
-                          ),
-                          //증가 버튼
-                        ],
-                      ), //제목 중앙
-                      trailing: SizedBox(
-                        width: 90,
-                        height: 80,
-                        child: Stack(
-                          children: [
-                            Align(
-                              alignment: Alignment.topRight,
-                              child: GestureDetector(
-                                onTap: () {
-                                  removeCartList(index);
-                                },
-                                child: Icon(
-                                  Icons.close,
-                                  size: 24,
-                                ),
-                              ),
-                            ), //제거버튼
-                            Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Text(
-                                '${item['price'] * item['quantity']}원',
-                              ),
-                            ), //가격표시
-                          ],
+              child: cartItems.isEmpty //장바구니가 비어 있을 때 문구 추가
+                  ? const Center(
+                      child: Text(
+                        '장바구니가 비어있습니다',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
                         ),
                       ),
+                    )
+                  : ListView.builder(
+                      itemCount: cartItems.length, //리스트에 담겨 있는 아이템 갯수만큼 생성해주기
+                      itemBuilder: (context, index) {
+                        final item = cartItems[index];
+                        return Card(
+                          //테두리를 사용하고 기본 레이아웃을 사용해 UI 구성
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                  color: Colors.black38, width: 1),
+                              borderRadius: BorderRadius.circular(10)),
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 20), //카드 여백
+                          child: ListTile(
+                            leading: item['image'] != null //왼쪽 이미지
+                                ? Image.file(
+                                    item[
+                                        'image']!, //링크를 사용 할 경우 network로 바꿔서 사용
+                                    width: 50,
+                                    height: 50,
+                                    fit: BoxFit.cover)
+                                : const Icon(
+                                    //이미지 파일이 없을경우
+                                    Icons.photo,
+                                    size: 50,
+                                    color: Colors.grey,
+                                  ),
+                            title: Text(item['title']),
+                            subtitle: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: () => decreaseQuantity(index),
+                                  icon: const Icon(Icons.remove),
+                                ),
+                                //감소 버튼
+                                Text('${item['quantity']}'), //수량
+                                IconButton(
+                                  onPressed: () => increaseQuantity(index),
+                                  icon: const Icon(Icons.add),
+                                ),
+                                //증가 버튼
+                              ],
+                            ), //제목 중앙
+                            trailing: SizedBox(
+                              width: 90,
+                              height: 80,
+                              child: Stack(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.topRight,
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        removeCartList(index);
+                                      },
+                                      child: Icon(
+                                        Icons.close,
+                                        size: 24,
+                                      ),
+                                    ),
+                                  ), //제거버튼
+                                  Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: Text(
+                                      '${item['price'] * item['quantity']}원',
+                                    ),
+                                  ), //가격표시
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () {}, //TODO 장바구니 구매 기능 구현
+            if (cartItems.isNotEmpty) //장바구니가 비어 있지 않을때 활성화 되도록 조건문 추가
+              Container(
+                padding: const EdgeInsets.all(20),
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: showDialog, //TODO 장바구니 구매 기능 구현
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.black,
                   ),
                   child: const Text(
                     '구매하기',
                     style: TextStyle(color: Colors.white, fontSize: 16),
-                  )),
-            )
+                  ),
+                ),
+              ),
           ],
         ),
       ),
