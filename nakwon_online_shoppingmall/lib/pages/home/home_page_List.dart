91783f180277d4f65b5b921190003bd7322d09f7
CHA BU GON <54930006+Dev-Combu@users.noreply.github.com>
2024-11-26 12:21:08 +0900

    Revert "[add] images 추가"

--- nakwon_online_shoppingmall/lib/pages/home/home_page_List.dart
+++ nakwon_online_shoppingmall/lib/pages/home/home_page_List.dart
@@ -1,56 +1,52 @@
 import 'package:nakwon_online_shoppingmall/album.dart';
 
 List<Album> album = [
-    Album(
-        imagePath: 'assets/images/peppertones.jpg',
-        song: '행운을 빌어요',
-        artist: '페퍼톤스',
-        price: 30000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/hangronan.jpg',
-        song: '오디세이',
-        artist: '행로난',
-        price: 50000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/surl.jpg',
-        song: 'Lights behind you',
-        artist: 'SURL',
-        price: 40000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/yudabinband.jpg',
-        song: '좋지 아니한가?',
-        artist: '유다빈밴드',
-        price: 15000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/theblackskirts.jpg',
-        song: '검정치마',
-        artist: 'LingLing',
-        price: 25000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/bandnah.jpg',
-        song: '각자의 밤',
-        artist: '나상현씨 밴드',
-        price: 40000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/rose.JPG',
-        song: 'APT.',
-        artist: 'ROSE, Bruno Mars',
-        price: 40000,
-        description: ''),
-    Album(
-        imagePath: 'assets/images/btob.jpg',
-        song: '노래',
-        artist: '비투비',
-        price: 40000,
-        description: ''),
   Album(
-      imagePath: 'assets/images/danicalifornia.jpg',
+      imagePath: '',
+      song: '행운을 빌어요',
+      artist: '페퍼톤스',
+      price: 30000,
+      description: ''),
+  Album(
+      imagePath: '',
+      song: '오디세이',
+      artist: '행로난',
+      price: 50000,
+      description: ''),
+  Album(
+      imagePath: '',
+      song: 'Lights behind you',
+      artist: 'SURL',
+      price: 40000,
+      description: ''),
+  Album(
+      imagePath: '',
+      song: '좋지 아니한가?',
+      artist: '유다빈밴드',
+      price: 15000,
+      description: ''),
+  Album(
+      imagePath: '',
+      song: '검정치마',
+      artist: 'LingLing',
+      price: 25000,
+      description: ''),
+  Album(
+      imagePath: '',
+      song: '각자의 밤',
+      artist: '나상현씨 밴드',
+      price: 40000,
+      description: ''),
+  Album(
+      imagePath: '',
+      song: 'APT.',
+      artist: 'ROSE, Bruno Mars',
+      price: 40000,
+      description: ''),
+  Album(
+      imagePath: '', song: '노래', artist: '비투비', price: 40000, description: ''),
+  Album(
+      imagePath: '',
       song: 'Dani California',
       artist: 'Red Hot Chili Peppers',
       price: 40000,
