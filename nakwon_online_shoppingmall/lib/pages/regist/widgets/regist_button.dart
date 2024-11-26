298e65c556858c8c45649b99371b3139874ab1ee
marion419 <daynvi@gmail.com>
2024-11-27 02:25:52 +0900

    [add]상품 등록할 때 메세지 출력되도록

--- nakwon_online_shoppingmall/lib/pages/regist/widgets/regist_button.dart
+++ nakwon_online_shoppingmall/lib/pages/regist/widgets/regist_button.dart
@@ -27,20 +27,46 @@ class RegistButton extends StatelessWidget {
                 widget.artist != null &&
                 widget.price != null &&
                 widget.description != null) {
-              // Album 객체 생성해서 pop
-              Navigator.pop(
-                  context,
-                  Album(
-                      imagePath: image!.path,
-                      song: widget.song!,
-                      artist: widget.artist!,
-                      price: widget.price!,
-                      description: widget.description!));
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('상품을 등록하였습니다'),
-                ),
-              );
+              // 등록 여부 확인 alert
+              showCupertinoDialog(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                        title: Text('상품을 등록하시겠습니까?'),
+                        actions: <CupertinoDialogAction>[
+                          // 취소 버튼
+                          CupertinoDialogAction(
+                            isDestructiveAction: true,
+                            textStyle: TextStyle(color: Colors.redAccent),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('취소'),
+                          ),
+                          // 확인 버튼
+                          CupertinoDialogAction(
+                              isDefaultAction: true,
+                              textStyle: TextStyle(color: Colors.black),
+                              onPressed: () {
+                                Navigator.pop(context);
+                                // Album 객체 생성해서 pop
+                                Navigator.pop(
+                                    context,
+                                    Album(
+                                        imagePath: image!.path,
+                                        song: widget.song!,
+                                        artist: widget.artist!,
+                                        price: widget.price!,
+                                        description: widget.description!));
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('상품을 등록하였습니다'),
+                                  ),
+                                );
+                              },
+                              child: Text('확인'))
+                        ],
+                      ));
+
               // 데이터를 모두 입력하지 않은 경우 경고문 출력
             } else {
               showCupertinoDialog(
